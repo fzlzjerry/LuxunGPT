f69cc4f5c2239594fd141969fbd20a75c8868e8f
Morax <James20081204@gmail.com>
2024-07-22 07:04:04 +0800

    写了模型和分词器

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ import random
 import numpy as np
 import torch
 from transformers import GPT2LMHeadModel, GPT2Tokenizer, Trainer, TrainingArguments, TextDataset, DataCollatorForLanguageModeling
+
+# Load the model and tokenizer
+model = GPT2LMHeadModel.from_pretrained('gpt2');
+tokenizer = GPT2Tokenizer.from_pretrained(('gpt2'));
+
